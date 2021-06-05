b94048cae73852165a2d459fb2e43740c3baff29
Çağatay Balıkçı <30704478+cagataybalikci@users.noreply.github.com>
2021-06-05 19:06:10 +0300

    Create Page created and routed in app.js

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"))
 
+let day = date()
+
 app.get("/", (req, res) => {
 
-    let day = date()
+    
     res.render("index", {currentDate : day})
 });
+
+app.get("/create",(req,res)=>{
+    res.render("create", {currentDate : day})
+})
 
 app.listen(3000, () => {
   console.log("Server start at port 3000.");
